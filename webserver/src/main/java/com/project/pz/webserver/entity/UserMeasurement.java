934a900334b6252e97d9049e58d03c7d359dc725
Piotr Sołtysiak <pi10521@alior.corp>
2016-05-18 13:47:46 +0200

    Added create-db script, Measurement entities and repositories

---
+++ webserver/src/main/java/com/project/pz/webserver/entity/UserMeasurement.java
@@ -0,0 +1,7 @@
+package com.project.pz.webserver.entity;
+
+/**
+ * Created by pi10521 on 2016-05-18.
+ */
+public class UserMeasurement {
+}
---
+++ webserver/src/main/java/com/project/pz/webserver/repository/UserMeasurementRepository.java
@@ -0,0 +1,7 @@
+package com.project.pz.webserver.repository;
+
+/**
+ * Created by pi10521 on 2016-05-18.
+ */
+public class UserMeasurementRepository {
+}
