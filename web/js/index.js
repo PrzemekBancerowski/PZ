7aba82cfdcb4f957b6f4c109b707f39eb8b2f369
mozzer39 <bartlomiej.trzewiczek@gmail.com>
2016-06-14 17:44:10 +0200

    pobieranie listy monitorow

---
+++ web/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$.ajax({
+		type: "GET",
+        url: "http://localhost:7755/monitors",
+		success: function(data){
+            $.each(data, function(i, val) {
+				$('#wybierzMonitor').append($('<option>').text(val.name + " (" + val.address + ")").attr('value', val.id));
+			});
+        },
+        error: function(){
+            $("#Warning").html("Błąd serwera.");
+        }
+    });
+});
+
+function chooseMonitor(){
+	id = $('#wybierzMonitor').val();
+	
+
+}
