c32fe78b752a714c6654e8786dde2f5fde32bb7b
Piotr Sołtysiak <piotrek.soltysiak@gmail.com>
2016-06-15 19:22:30 +0200

    poprawki do wara

--- webserver/src/main/java/com/project/pz/webserver/App.java
+++ webserver/src/main/java/com/project/pz/webserver/App.java
@@ -2,13 +2,20 @@ package com.project.pz.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by Piotr Sołtysiak on 2016-05-06.
  * Contact: piotrek.soltysiak@gmail.com
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
