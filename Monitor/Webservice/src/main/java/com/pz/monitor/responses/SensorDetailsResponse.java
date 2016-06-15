4b209db4d7cae89b9c664de9aee06069a03c1930
sebastian lipnicki <aragorn20@poczta.onet.pl>
2016-06-15 20:59:34 +0200

    change sensor response format

--- Monitor/Webservice/src/main/java/com/pz/monitor/responses/SensorDetailsResponse.java
+++ Monitor/Webservice/src/main/java/com/pz/monitor/responses/SensorDetailsResponse.java
@@ -2,26 +2,28 @@ package com.pz.monitor.responses;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class SensorDetailsResponse {
     private final String id;
-    private final String sysName;
-    private final String sysVendor;
-    private final String sysVersion;
+    private final String systemName;
+    private final String systemVendor;
+    private final String systemVersion;
     private final String systemArch;
     private final String cpu;
     private final String cpuVendor;
     private final String cpuCoreCount;
     private final String hostName;
     private final String Dns;
+    private final int memorySize;
     private final String CreatedOn;
 
     public SensorDetailsResponse(ResultSet resultSet) throws SQLException {
         if (resultSet.next()) {
             this.id= resultSet.getString("sensorId");
-            this.sysName= resultSet.getString("name");
-            this.sysVendor= resultSet.getString("vendor");
-            this.sysVersion= resultSet.getString("version");
+            this.systemName= resultSet.getString("name");
+            this.systemVendor= resultSet.getString("vendor");
+            this.systemVersion= resultSet.getString("version");
             this.systemArch= resultSet.getString("architecture");
             this.cpu= resultSet.getString("cpu_model");
             this.cpuVendor= resultSet.getString("cpu_vendor");
@@ -29,6 +31,7 @@ public class SensorDetailsResponse {
             this.hostName= resultSet.getString("hostname");
             this.Dns = resultSet.getString("dns");
             this.CreatedOn = resultSet.getString("createdOn");
+            this.memorySize = new Random().nextInt();
         } else {
             throw new IllegalArgumentException("Invalid sensorId");
         }
